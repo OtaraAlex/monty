977e734cc9aea2e1237778aeeabc0c2ddd54e618
OtaraAlex <alexotara61@gmail.com>
2022-12-14 05:18:35 -0800

    add

---
+++ add.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * add - add node
+ * @stack: pointer to the tp of stack
+ * @line_number: number of line
+ *
+ * Return: void
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+	int sum;
+
+	if (!stack || !(*stack) || !(*stack)->next)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *stack;
+
+	sum = temp->n + temp->next->n;
+	temp->next->n = sum;
+
+	*stack = temp->next;
+
+	free(temp);
+}
