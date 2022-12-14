c6be7e73d73cc8c426e763955eed07a3e2c81fa2
OtaraAlex <alexotara61@gmail.com>
2022-12-14 05:20:44 -0800

    nop

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - implement the nop function
+ * @stack: stack
+ * @line_number: line number
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
